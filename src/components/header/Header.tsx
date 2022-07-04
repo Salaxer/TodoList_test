dc5cdd8641fa3f9a1713ddb31080e2b3e400a1eb
Hector <contact@salaxer.com>
2022-07-04 15:32:34 -0500

    [init]:start Pretty todoList

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const Header = () => {
+
+  return (
+    <nav>
+
+    </nav>
+  )
+}
+
+export default Header
